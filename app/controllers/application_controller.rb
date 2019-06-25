7438bbbee0049ca68b98a557652c398af4b39bcb
Daniela Fernandez <daniela.fernandez7@gmail.com>
2019-06-24 22:03:35 -0400

    primer commit

---
+++ app/controllers/application_controller.rb
@@ -0,0 +1,7 @@
+class ApplicationController < ActionController::Base
+  protect_from_forgery with: :exception
+  private
+  def authenticate_user!
+    redirect_to users_sign_in_path unless helpers.looged?
+  end
+end
