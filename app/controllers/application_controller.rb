a7e91a6716e8486f4d56527912250eaa67c9dbd6
Daniela Fernandez <daniela.fernandez7@gmail.com>
2019-06-25 23:15:46 -0400

    modificaciones

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,3 @@
 class ApplicationController < ActionController::Base
-  protect_from_forgery with: :exception
-  private
-  def authenticate_user!
-    redirect_to users_sign_in_path unless helpers.looged?
-  end
+
 end
