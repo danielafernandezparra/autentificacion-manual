4996bbb5e6f94668b7ad0ada8b0acbafab39c299
Daniela Fernandez <daniela.fernandez7@gmail.com>
2019-06-25 23:33:17 -0400

    ultimas correciones

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,6 +3,6 @@ class ApplicationController < ActionController::Base
 
   private
   def authenticate_user!
-    redirect_to users_sing_in_path unless helpers.logged?
+    redirect_to users_sign_in_path unless helpers.logged?
   end
 end
